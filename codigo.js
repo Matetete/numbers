64570b28fdabe59c0a0614ea12edbf822e47e514
Matías <barmat520@gmail.com>
2021-07-10 00:55:43 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,13 @@
+let numero = prompt("Coloque un número")
+
+if (numero >= 0 && numero <= 9) {
+	alert("Su número esta entre 0 y 9")
+}
+
+else if (numero >= 10 && numero <= 20) {
+	alert("Su número esta entre 10 y 20")
+}
+
+else {
+	alert("Su número no aparece en mi sistema")
+}
